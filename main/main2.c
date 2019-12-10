9bc117233573d29911b7fe6dd9a9c7b48e774cb2
Eithan ASSOULINE <eassouli@e2r9p8.42.fr>
2019-12-10 05:45:58 +0100

    add new tests and move directory

--- main/main2.c
+++ main/main2.c
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/09 20:54:25 by eassouli          #+#    #+#             */
-/*   Updated: 2019/12/09 20:55:44 by eassouli         ###   ########.fr       */
+/*   Created: 2019/12/09 20:48:49 by eassouli          #+#    #+#             */
+/*   Updated: 2019/12/10 05:43:57 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include <stdio.h>
 
 int	main(void)
 {
-	ft_printf("Nombre de caracteres : %d", ft_printf("%conjour%s%ld\nsalut\n%sok           %d", 'B', " toi\n", -2147483648, "it's ", 10));
+	printf("salut\n");
+	printf("%conjour%s%ld\nsalut\n%sok           %d", 'B', " toi\n", -2147483648, "it's ", 10);
+	printf("\n%%     \n");
+	printf("salut%ld\n", -9223372036854775807);
+	printf("%d", printf("salut%ld\n", 1844674407370955161));
 	return (0);
 }
